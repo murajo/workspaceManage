34cd0c281df88c04b3a3bcc734732570c91eccfd
murajo <t5000carbon@docomo.ne.jp>
2018-06-28 10:15:49 +0900

    指定したアプリのワークスペースの場所を返すメソッド

--- xdotool.py
+++ xdotool.py
@@ -1,23 +1,40 @@
 import subprocess
-import os
-import sys
 
 # 指定したアプリケーションのwindowIDを配列で返す
 # return array(str)
 def get_window_id(name):
-    res = subprocess.check_output(["xdotool", "search", "--name", name])
-    split_num_array = str(res).strip("b'").split("\\n")[:-1]
-    return split_num_array
+    try:
+        res = subprocess.check_output(["xdotool", "search", "--name", name])
+        res = str(res).strip("b'").split("\\n")[:-1]
+    except:
+        res = ["-1"]
+    return res
 
 # 指定したwindowIDのワークスペースを変更
-# return int
+# return intxdo.get_workspace_num(id))
 def set_window_workspace(window_id, work_num):
-    res = subprocess.check_call(["xdotool", "set_desktop_for_window", window_id, work_num])
+    try:
+        res = subprocess.check_call(["xdotool", "set_desktop_for_window", window_id, work_num])
+    except:
+        res = -1
     return res
 
 # ワークスペースの数を返す
 # return str
 def get_workspace_count():
-    count = subprocess.check_output(["xdotool", "get_num_desktops"])
-    count = str(count).strip("b'\\n")
-    return count
+    try:
+        res = subprocess.check_output(["xdotool", "get_num_desktops"])
+        res = str(res).strip("b'\\n")
+    except:
+        res = "-1"
+    return res
+
+# 指定したwindowIDのワークスペースの場所を表示
+# return int
+def get_workspace_num(window_id):
+    try:
+        res = subprocess.check_output(["xdotool", "get_desktop_for_window", window_id])
+        res = int(str(res).strip("b'\\n"))
+    except:
+        res = -1
+    return res
